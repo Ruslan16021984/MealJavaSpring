02535eb4918445656a7f128ae304ae1a15c0f417
Ruslan16021984 <carbit3333333@gmail.com>
2019-12-24 14:33:17 +0200

    commit2

--- src/main/java/com/gmail/carbit/model/UserMealWithExcess.java
+++ src/main/java/com/gmail/carbit/model/UserMealWithExcess.java
@@ -4,21 +4,40 @@ import java.time.LocalDateTime;
 
 public class UserMealWithExcess {
     private final LocalDateTime dateTime;
+    private final String description;
     private final int calories;
     private final boolean exess;
 
-    public UserMealWithExcess(LocalDateTime dateTime, int calories, boolean exess) {
+    public UserMealWithExcess(LocalDateTime dateTime, String description, int calories, boolean exess) {
         this.dateTime = dateTime;
         this.calories = calories;
         this.exess = exess;
+        this.description= description;
     }
 
     @Override
     public String toString() {
-        return "com.gmail.carbit.model.UserMealWithExcess{" +
+        return "UserMealWithExcess{" +
                 "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
                 ", calories=" + calories +
                 ", exess=" + exess +
                 '}';
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExess() {
+        return exess;
     }
 }
--- src/main/java/com/gmail/carbit/util/UserMealsUtils.java
+++ src/main/java/com/gmail/carbit/util/UserMealsUtils.java
@@ -3,11 +3,14 @@ package com.gmail.carbit.util;
 import com.gmail.carbit.model.UserMeal;
 import com.gmail.carbit.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class UserMealsUtils {
     public static void main(String[] args) {
@@ -18,17 +21,29 @@ public class UserMealsUtils {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+        );
+
+        List<UserMealWithExcess> mealsTo = filteredByStrim(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
     }
 
-    public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
+    public static List<UserMealWithExcess> filteredByStrim(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        Map<LocalDate, Integer> caloriesSumByDate = meals.stream().collect(Collectors.groupingBy(um -> um.getDateTime().toLocalDate(),
+                Collectors.summingInt(UserMeal::getCalories)));
+       return meals.stream().
+                filter(um-> TimeUtil.isBetweenInclusive(um.getDateTime()
+                        .toLocalTime(), startTime, endTime))
+                .map(um-> new UserMealWithExcess(um.getDateTime()
+                        , um.getDescription()
+                        , um.getCalories(),caloriesSumByDate.get(um.getDateTime().toLocalDate())> caloriesPerDay))
+                .collect(Collectors.toList());
     }
 
-    public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+    public static List<UserMealWithExcess> filteredByCycle(List<UserMeal> meals,
+                                                           LocalTime startTime,
+                                                           LocalTime endTime,
+                                                           int caloriesPerDay) {
         // TODO Implement by streams
         return null;
     }
